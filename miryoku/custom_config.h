254744d5152b17f3437be7a1e479536122c78d42
Micah Loffer <micah.loffer@protonmail.com>
2025-05-25 20:05:03 -0700

    Add modifiers to extra keys

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -11,9 +11,9 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-&kp X  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp X \
-&kp X  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp X \
-&kp X  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp X \
+&kp LCTRL  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &kp RCTRL \
+&kp LSHFT  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp RSHFT \
+&kp LALT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &kp RALT \
                K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
